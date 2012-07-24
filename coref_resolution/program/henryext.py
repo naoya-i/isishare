889b98121c86faf1545a1bf9e9a58fe9cdc81ac2
Naoya Inoue@Whiskey <naoya-i@ecei.tohoku.ac.jp>
2012-07-24 13:51:46 +0900

    Non-identity explicit predicates.

--- coref_resolution/program/henryext.py
+++ coref_resolution/program/henryext.py
@@ -1,10 +1,9 @@
 #
-# Henry extension module for coreference experiment
+# Henry external module for coreference experiment
 #
 
 import sys, re, os, math
 
-from lxml import etree
 from collections import defaultdict
 
 def _myfile( x ):
@@ -12,12 +11,61 @@ def _myfile( x ):
 
 g_mydir		 = os.path.abspath(os.path.dirname(__file__))
 
-g_cu        = \
-		re.findall( "set_condition\((.*?)'/", open( _myfile( "../data/cu-boxer.kb" ) ).read() ) + \
+#
+# Read the resources.
+g_cu							 =																																				 \
+		re.findall( "set_condition\((.*?)'/", open( _myfile( "../data/cu-boxer.kb" ) ).read() ) +\
 		re.findall( "set_condition\((.*?)'/", open( _myfile( "../data/cu-adj.kb" ) ).read() )
-g_word_freq = dict( [(x.split()[1], int(x.split()[3])) for x in open( _myfile( "../data/entriesWithoutCollocates.txt" ) )] )
-g_disj = {}
+g_word_freq				 = dict( [(x.split()[1], int(x.split()[3])) for x in open( _myfile( "../data/entriesWithoutCollocates.txt" ) )] )
+g_disj						 = {}
+g_explicit_non_ids = []
 
+for ln in open( _myfile( "../data/inequality.kb" ) ):
+	ret = re.findall( " ([^ ]+)'\((.*?)\)", ln.strip() )
+	if 0 == len(ret): continue
+
+	atoms	= [(x, y.split(",").index( "x" ) if "x" in y else -1, y.split(",").index( "y" ) if "y" in y else -1 ) for x, y in ret]
+	mapper = defaultdict( list )
+
+	# For isomorphic processing...
+	if 1 == len(atoms): atoms += [atoms[0]]
+	
+	# Which arguments are expected to same?
+	for predicate, args in ret:
+		for i, arg in enumerate( args.split(",") ):
+			mapper[ arg ] += [i]
+	
+	g_explicit_non_ids += [(atoms, dict( filter( lambda x: 1<len(x[1]), mapper.iteritems() ) ) )]
+	
+	
+def _isExplicitNonIdent( p1p, p1a, p2p, p2a, ti, tip, tj, tjp ):
+	
+	for atoms, mapper in g_explicit_non_ids:
+
+		if (atoms[0][0] == p1p and atoms[1][0] == p2p) or (atoms[0][0] == p2p and atoms[1][0] == p1p):
+
+			if not ( (tip == atoms[0][1] and tjp == atoms[1][2]) or (tjp == atoms[0][1] and tip == atoms[1][2]) ): continue
+			
+			# Are the arguments same?
+			for arg, pos in mapper.iteritems():
+				if p1a.split(",")[ pos[0] ] != p2a.split(",")[ pos[1] ]: break
+			else:
+				return True
+
+	return False
+
+
+def _isEventArg( a, v2h ):
+	for p in v2h.get( a, [] ):
+		if "(%s," % a in p: return True
+	return False
+
+
+def _getNonFirstEventArg( pa, v2h ):
+	for arg in pa.split(",")[1:]:
+		if _isEventArg(arg, v2h): return arg
+
+		
 #
 # This is a callback function that decides how much two literals li
 # and lj are evidential for the unification ti=tj.
@@ -26,41 +74,67 @@ g_disj = {}
 #  ti, tj:         logical terms to be unified.
 #  v2h:            mapping from logical terms to potential elemental hypotheses.
 #
-def cbGetUnificationEvidence( ti, tj, v2h ):
+def cbGetUnificationEvidence( ti, tj, v2h, shallow_search=False ):
 
 	# Different constants cannot be unified.
 	if ti != tj and ti[0].isupper() and tj[0].isupper(): return []
-		
+
 	def _getArgPos( p, t ):
 		return re.split( "[(,)]", p ).index(t) - 1
 	
 	ret = []
 	
 	for p1 in v2h.get( ti, [] ):
-		p1	 = p1.split( ":" )
-		p1id = int(p1[-1])
+		p1l, p1id = p1.split( ":" )
+		p1p, p1a	= p1l.split( "(" )
+		p1a, p1id	= p1a[:-1], int(p1id)
+		tip       = _getArgPos( p1l, ti )
 		
 		for p2 in v2h.get( tj, [] ):
-			p2	 = p2.split( ":" )
-			p2id = int(p2[-1])
+			p2l, p2id = p2.split( ":" )
+			p2p, p2a	= p2l.split( "(" )
+			p2a, p2id	= p2a[:-1], int(p2id)
+			tjp       = _getArgPos( p2l, tj )
+			
+			# if ti != tj and _isExplicitNonIdent( p1p, p1a, p2p, p2a, ti, tip, tj, tjp ):
+				
+			# 	# Ok, identifying coreference relation b/w ti and tj seems not good.
+			# 	ret += [(-10, [p1id, p2id])]
+			# 	continue
 			
 			#
 			if p1id == p2id or (ti == tj and p1id > p2id): continue
-			
+
 			# Evidence provided by the same predicate
-			if ti != tj and _getArgPos( p1[0], ti ) == _getArgPos( p2[0], tj ) and p1[0].split("(")[0] == p2[0].split("(")[0]:
+			if ti != tj and p1p == p2p and tip == tjp:
 				
 				# # Is this really evidence for coreference?
-				if p1[0].split("(")[0] in g_cu:           continue # Conditional unification predicates.
-				if p1[0].split("(")[0].endswith( "-in" ): continue # Prepositional phrase.
-				if p1[0].split("(")[0].endswith( "-rb" ): continue # Adverbs.
+				if p1p in g_cu:           continue # Conditional unification predicates.
+				# if p1p.endswith( "-in" ): continue # Prepositional phrase.
+				# if p1p.endswith( "-rb" ): continue # Adverbs.
 
+				# # Are ti and tj non-first event arguments?
+				# if _isEventArg(ti, v2h) and _isEventArg(tj, v2h) and 0 != tip and 0 != tjp:
+				# 	continue
+
+				# # Is ti and tj the first event argument of proposition that have non-first event arguments?
+				# nfe1, nfe2 = _getNonFirstEventArg(p1l, v2h), _getNonFirstEventArg(p2l, v2h)
+
+				# if _isEventArg(ti, v2h) and _isEventArg(tj, v2h) and None != nfe1 and None != nfe2:
+
+				# 	# The evidence is provided by literals that have the non-first event arguments, instead of the occurrence of p1 and p2.
+				# 	if not shallow_search: ret += cbGetUnificationEvidence( nfe1, nfe2, v2h )
+					
+				#  	continue
+				
 				# Otherwise, return how frequent the word is.
-				ret += [ (-0.1*math.log( g_word_freq.get( p1[0].split("-")[0], 2 ) ), [p1id, p2id]) ]
+				ret += [ (-1.0*math.log( g_word_freq.get( p1p, 2 ) ), [p1id, p2id]) ]
 
 			# # Disjoint?
-			if g_disj.has_key("%s/1%s/1" % (p1[0].split("(")[0], p2[0].split("(")[0])) or g_disj.has_key("%s/1%s/1" % (p1[0].split("(")[0], p2[0].split("(")[0])):
-				ret += [ (-9999, [p1id, p2id] ) ]
+			# if g_disj.has_key("%s/1%s/1" % (p1p, p2p)) or g_disj.has_key("%s/1%s/1" % (p1p, p2p)):
+			# 	ret += [ (-9999, [p1id, p2id] ) ]
 
+	#print ti, tj, ret
+	
 	return ret
 
---
+++ plan_recognition/program/mooney-o2henry.py
@@ -0,0 +1,50 @@
+
+import argparse, sys, re
+
+def _constantify(x):
+	x = x.split( " " )
+	return "%s %s" % (x[0], " ".join( [v.upper() if not v.startswith("?") else v for v in x[1:]] ))
+
+
+def main():
+	parser = argparse.ArgumentParser( description="Ng & Mooney to Henry converter." )
+	parser.add_argument( "--input", help="Lisp files to be converted.", type=file, nargs="+", default=sys.stdin )
+	parser.add_argument( "--tlp", help="Top level plan info file.", type=file, nargs=1 )
+	pa = parser.parse_args()
+
+	if None == pa.tlp: parser.error( "Where is the top-level plan files?" )
+	
+	TopLevelPlans = re.findall("\(plan_.*? (.*?) ", pa.tlp[0].read() )
+
+	for f in pa.input:
+		train = {}
+
+		for name_prefix, name_no, content in re.findall( "\(setf \*([tea]+)([0-9]+).*?'\((.*?)\)\)\n", f.read(), re.DOTALL ):
+
+			lfs = re.findall( "\((.*?)\)", re.sub( "inst ([^ ]+) ([^\)]+)", "inst_\\2 \\1", content ).replace( "-", "_" ) )
+			lfs = ["      (%s :10)" % _constantify(l) for l in lfs]
+
+			if "a" == name_prefix[0]:
+
+				# Add negative label to avoid hypothesize top-level plan literals
+				for tlp in TopLevelPlans:
+					if "inst_%s" % tlp not in [x.split()[0][1:] for x in lfs]:
+						lfs += ["      (! (inst_%s *))" % tlp]
+
+			lfs = "\n".join( lfs )
+
+			if not train.has_key( name_no ): train[ name_no ] = {}
+
+			train[ name_no ][ "gold" if "a" == name_prefix[0] else "obs" ] = name_prefix, lfs
+
+
+		for problem in sorted( train, key=lambda x: int(x) ):
+			print "(O (name %s%s) (label (structure (^" % (train[problem]["obs"][0], problem)
+			print "%s" % (train[problem]["gold"][1])
+			print "     ) ) ) (^ "
+			print train[problem]["obs"][1]
+			print "     ) )"
+			print
+	
+	
+if "__main__" == __name__: main()
